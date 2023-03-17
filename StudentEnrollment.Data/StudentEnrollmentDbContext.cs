e86c7bec904b6ac332ed9d3127c96e76abf5b080
OlahRobert <olahrobert21@gmail.com>
2023-03-17 14:29:24 +0200

    New endpoints for student, enrollment, course

--- StudentEnrollment.Data/StudentEnrollmentDbContext.cs
+++ StudentEnrollment.Data/StudentEnrollmentDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using StudentEnrollment.Data.Configurations;
 using System;
 using System.Collections.Generic;
@@ -26,5 +28,23 @@ namespace StudentEnrollment.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+    }
+
+    public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<StudentEnrollmentDbContext>
+    {
+        public StudentEnrollmentDbContext CreateDbContext(string[] args)
+        {
+            //Build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            //GEt connection string
+            var optionsBuilder = new DbContextOptionsBuilder<StudentEnrollmentDbContext>();
+            var connectionString = config.GetConnectionString("StudenEnrollmentDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new StudentEnrollmentDbContext(optionsBuilder.Options);
+        }
     }
 }
